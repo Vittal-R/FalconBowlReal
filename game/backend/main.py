630105daaebfc1fcfb91033576dfd9febf9b4028
Vittal Rajagopalan <vittal.rajagopalan@gmail.com>
2025-09-10 19:30:48 -0400

    set up python environment fastApi backend and vite frontend

---
+++ game/backend/main.py
@@ -0,0 +1,11 @@
+print("Hello")
+from fastapi import FastAPI, APIRouter
+
+app = FastAPI()
+
+router = APIRouter()
+@router.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+app.include_router(router)
